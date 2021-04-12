50b343c43ef5c61570d0f0d880ec6d7c467e360f
= <=>
2021-04-13 00:39:34 +0900

    add valid_mapcheck func in main.c

--- srcs/main.c
+++ srcs/main.c
@@ -6,24 +6,19 @@
 /*   By: kalee <kalee@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/10 23:19:51 by kalee             #+#    #+#             */
-/*   Updated: 2021/04/12 18:06:05 by kalee            ###   ########.fr       */
+/*   Updated: 2021/04/13 00:38:31 by kalee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_bsq_essential.h"
 
-void	handle_error(char *msg)
+void	bsq_logic(char *filename, char *charset, t_point *pts)
 {
-	ft_putstr(msg);
-	write(1, "\n", 1);
-	exit(1);
-}
-
-void	bsq_logic(int fd, char *charset, t_point *pts)
-{
+	int		fd;
 	char	**map;
 	int		**parsed_map;
 
+	fd = open(filename, O_RDONLY);
 	map = read_map(fd, pts);
 	close(fd);
 	parsed_map = parse_map(map, pts, charset);
@@ -32,14 +27,35 @@ void	bsq_logic(int fd, char *charset, t_point *pts)
 	print_map(map, pts[0].x, pts[0].y);
 	free_intmap(parsed_map, pts[0].y);
 	free_charmap(map, pts[0].y);
-	free(pts);
+}
+
+int		io_process(char *filename, char *charset, t_point *pts)
+{
+	int	fd;
+	int	idx;
+	int	flag;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (-1);
+	pts[0].y = get_map_height(fd, charset, &idx);
+	close(fd);
+	if (pts[0].y <= 0)
+		return (-1);
+	fd = open(filename, O_RDONLY);
+	pts[0].x = get_map_width(fd, idx);
+	close(fd);
+	fd = open(filename, O_RDONLY);
+	flag = valid_mapcheck(fd, pts[0].x, pts[0].y);
+	close(fd);
+	if (flag == -1)
+		return (-1);
 }
 
 int		main(int argc, char **argv)
 {
 	int		i;
-	int		idx;
-	int		fd;
+	int		flag;
 	char	charset[3];
 	t_point	*pts;
 
@@ -47,18 +63,14 @@ int		main(int argc, char **argv)
 	while (++i < argc)
 	{
 		pts = (t_point *)malloc(3 * sizeof(t_point));
-		fd = open(argv[i], O_RDONLY);
-		if (fd == -1)
-			handle_error("map file error: file is missing");
-		pts[0].y = get_map_height(fd, charset, &idx);
-		if (pts[0].y <= 0)
-			handle_error("map error");
-		close(fd);
-		fd = open(argv[i], O_RDONLY);
-		pts[0].x = get_map_width(fd, idx);
-		fd = open(argv[i], O_RDONLY);
-		bsq_logic(fd, charset, pts);
-		close(fd);
+		flag = io_process(argv[i], charset, pts);
+		if (flag == -1)
+		{
+			ft_putstr("map error\n");
+			continue;
+		}
+		bsq_logic(argv[i], charset, pts);
+		free(pts);
 	}
 	return (0);
 }
